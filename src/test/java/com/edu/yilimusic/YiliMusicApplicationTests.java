471aa95ba87de42050183d317b2635f4bf8d35a2
张航 <672683505@qq.com>
2022-07-10 21:15:48 +0800

    first commit

---
+++ src/test/java/com/edu/yilimusic/YiliMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.yilimusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YiliMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
