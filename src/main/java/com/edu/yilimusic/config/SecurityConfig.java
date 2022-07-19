514dfe881997950eceb3a3e9efae9bce469d8847
张航 <672683505@qq.com>
2022-07-19 21:10:05 +0800

    cc

--- src/main/java/com/edu/yilimusic/config/SecurityConfig.java
+++ src/main/java/com/edu/yilimusic/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .exceptionHandling()
 //                .authenticationEntryPoint(restAuthenticationEntryPoint)
 //                .and()
+
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
